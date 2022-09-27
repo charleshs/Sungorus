d4638c18fd60aef8f59718a9507ee67e316a0e9a
Charles Hsieh <charles.hsieh@crypto.com>
2022-09-28 02:39:34 +0800

    Refactor: rename methods

--- Sources/Sungorus/Extension/UIKit+Sungorus.swift
+++ Sources/Sungorus/Extension/UIKit+Sungorus.swift
@@ -2,52 +2,52 @@ import UIKit
 
 public extension UIViewController {
     @discardableResult
-    func sgr_navigate(task: NavigationTask, queue: NavigationQueue) -> NavigationTask {
+    func sgrEnqueueTask(_ task: NavigationTask, queue: NavigationQueue) -> NavigationTask {
         queue.add(task)
         return task
     }
 
     @discardableResult
-    func sgr_present(_ presentable: UIViewController, animated: Bool) -> NavigationTask {
+    func sgrEnqueuePresent(_ presentable: UIViewController, animated: Bool) -> NavigationTask {
         let task = PresentNavigationTask(source: self, presentable: presentable, animated: animated)
-        return sgr_navigate(task: task, queue: .main)
+        return sgrEnqueueTask(task, queue: .main)
     }
 
     @discardableResult
-    func sgr_dismiss(animated: Bool) -> NavigationTask {
+    func sgrEnqueueDismiss(animated: Bool) -> NavigationTask {
         let task = DismissNavigationTask(presented: self, animated: animated)
-        return sgr_navigate(task: task, queue: .main)
+        return sgrEnqueueTask(task, queue: .main)
     }
 
     @discardableResult
-    func sgr_dismissAsPresenter(animated: Bool) -> NavigationTask {
+    func sgrEnqueueDismissAsPresenter(animated: Bool) -> NavigationTask {
         let task = DismissNavigationTask(presentationContext: self, animated: animated)
-        return sgr_navigate(task: task, queue: .main)
+        return sgrEnqueueTask(task, queue: .main)
     }
 }
 
 public extension UINavigationController {
     @discardableResult
-    func sgr_pushViewController(_ viewController: UIViewController, animated: Bool) -> NavigationTask {
+    func sgrEnqueuePush(_ viewController: UIViewController, animated: Bool) -> NavigationTask {
         let task = PushNavigationTask(navigationController: self, viewController: viewController, animated: animated)
-        return sgr_navigate(task: task, queue: .main)
+        return sgrEnqueueTask(task, queue: .main)
     }
 
     @discardableResult
-    func sgr_popViewController(animated: Bool) -> NavigationTask {
+    func sgrEnqueuePop(animated: Bool) -> NavigationTask {
         let task = PopNavigationTask(navigationController: self, animated: animated)
-        return sgr_navigate(task: task, queue: .main)
+        return sgrEnqueueTask(task, queue: .main)
     }
 
     @discardableResult
-    func sgr_popToViewController(_ destination: UIViewController, animated: Bool) -> NavigationTask {
+    func sgrEnqueuePopTo(_ destination: UIViewController, animated: Bool) -> NavigationTask {
         let task = PopNavigationTask(navigationController: self, destination: destination, animated: animated)
-        return sgr_navigate(task: task, queue: .main)
+        return sgrEnqueueTask(task, queue: .main)
     }
 
     @discardableResult
-    func sgr_popToRootViewController(animated: Bool) -> NavigationTask {
+    func sgrEnqueuePopToRoot(animated: Bool) -> NavigationTask {
         let task = PopToRootNavigationTask(navigationController: self, animated: animated)
-        return sgr_navigate(task: task, queue: .main)
+        return sgrEnqueueTask(task, queue: .main)
     }
 }
